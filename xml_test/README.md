3e392a3720669166e2404d1a47a64b59c320f79d
RemyMach <remygt@hotmail.fr>
2020-11-21 16:38:22 +0100

    ajout README.md pour tests

---
+++ xml_test/README.md
@@ -0,0 +1,33 @@
+#MENU TEST
+
+##not_valid
+
+### **1**
+- pas valide car la balise root ne se ferme pas
+
+### **2**
+- pas valide car une seule balise xml et pas de balise root
+
+### **3**
+- fichier vide
+
+### **4**
+- la balise jean se ferme après la fermeture d'une de ses balises parent
+
+### **5**
+- la valeur de l'attribut contient des valeur qui ne faut pas et ne permet pas de fermer la balise root
+
+### **6**
+- il manque un = au dernier attribut, mauvaise typo
+
+### **7**
+- deux noms d'attributs pour une même balise sont les mêmes
+
+### **8**
+- mauvaise typo pour les noms d'attributs
+
+### **9**
+- une balise qui n'est pas fermé
+
+### **10**
+- un commentaire avant la balise xml rend le fichier xml non valide
--- xml_test/test_xml.c
+++ xml_test/test_xml.c
@@ -8,67 +8,15 @@
 int main() {
 
     int valid;
-    printf("not_valid_count -> %d\n", NOT_VALID_COUNT);
-
-    //for(int i=1;i<=NOT_VALID_COUNT; i++) {
-    for(int i=1;i<=2; i++) {
-        //char str[strlen(not_valid) + 6];
-        //char* str = malloc(sizeof(char) * ( strlen(not_valid) + 6 ) );
-        char* str = malloc(sizeof(char) * 26);
-        
-        printf("str -> %s\n", str);
-        sprintf(str, "xml_test/not_valid_%d.xml", i);
-        str[24] = '\0';
-        LinkedListTag* head = intialisation("");
-        printf("\n---------------------------------------------------------------\n");
-        printf("file ->%s\n", str);
-        printf("\n---------------------------------------------------------------\n");
-        valid = validateRead(head, str);
-        if(valid == 0) {
-            printf("le fichier xml n'est pas valide\n");
-            printf("le test est passé!\n");
-            
-        }else {
-            printf("le fichier est valide\n");
-            printf("le test n'est pas passé!\n");
-            return 0;
-        }
-        *str = NULL;
-        free(str);
-        if(strcmp(head->name, "") != 0) {
-            freeLinkedListTag(head);
-        }
-        //il faut vider la LinkedList head
-        //return 0;
+    LinkedListTag* head = intialisation("");
+    valid = validateRead(head, "xml_test/valid_9.xml");
+    freeLinkedListTag(head);
+    if(valid == 0) {
+        printf("le fichier xml n'est pas valide\n");
+    }else {
+        printf("le fichier est valide\n");
     }
-
-    //for(int i=1;i<=VALID_COUNT; i++) {
-    for(int i=1;i<=0; i++) {
-
-        //char str[strlen(not_valid) + 6];
-        char* str = malloc(sizeof(char) * ( 22 ) );
-
-        sprintf(str, "xml_test/valid_%d.xml", i);
-        str[21] = '\0';
-        LinkedListTag* head = intialisation("");
-        printf("\n---------------------------------------------------------------\n");
-        printf("file ->%s\n", str);
-        printf("\n---------------------------------------------------------------\n");
-        valid = validateRead(head, str);
-        if(valid == 0) {
-            printf("le fichier xml n'est pas valide\n");
-            printf("le test n'est pas passé!\n");
-            return 0;
-        }else {
-            printf("le fichier est valide\n");
-            printf("le test est passé!\n");
-        }
-        *str = NULL;
-        free(str);
-        freeLinkedListTag(head);
-        //il faut vider la LinkedList head
-        //return 0;
-    }
+    return 0;
 
     printf("\n---------------------------------------------------------------\n");
     printf("********** L'ensemble des tests sont passés **********");
